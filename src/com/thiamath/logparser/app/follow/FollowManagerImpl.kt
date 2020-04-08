fe755cbe2e61425bd859732965997e857be01908
Thiamath <thiamath@gmail.com>
2020-04-08 15:04:45 +0200

    Follower implementation.

--- src/com/thiamath/logparser/app/follow/FollowManagerImpl.kt
+++ src/com/thiamath/logparser/app/follow/FollowManagerImpl.kt
@@ -1,15 +1,118 @@
 package com.thiamath.logparser.app.follow
 
 import com.thiamath.logparser.app.model.FollowResult
+import com.thiamath.logparser.app.model.LogLine
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.IOException
+import java.lang.Thread.sleep
+import java.time.Clock
 import java.util.concurrent.BlockingQueue
+
 
 class FollowManagerImpl(
         override val resultQueue: BlockingQueue<FollowResult>,
-        val resultIntervalMs: Long = 60 * 60 * 1000 // ONE HOUR
+        val resultIntervalMs: Long = 60 * 60 * 1000, // ONE HOUR
+        var clock: Clock = Clock.systemUTC()
 ) : FollowManager {
 
-    override fun followParseFile(filename: String, hostname: String) {
+    private var followThread: Thread? = null
+    private var running = true
 
-        TODO("Not yet implemented")
+    override fun followParseFile(filename: String, hostname: String) {
+        var lastReadTimeMs = clock.millis()
+        followThread = Thread {
+            BufferedInputStream(File(filename).inputStream()).use {
+                firstRead(it, hostname)
+                val fromHostnameList = HashSet<String>()
+                val toHostnameList = HashSet<String>()
+                val hostnameActivity = HashMap<String, Int>()
+                while (running) {
+                    try {
+                        val line = readLine(it)
+                        if (line != "") {
+                            parseLine(line, hostname, toHostnameList, fromHostnameList, hostnameActivity)
+                        }
+                        try {
+                            sleep(100)
+                        } catch (ex: InterruptedException) {
+                            running = false
+                        }
+                        if (clock.millis() > lastReadTimeMs + resultIntervalMs) {
+                            resultQueue.put(FollowResult(
+                                    fromHostnameList.toList(),
+                                    toHostnameList.toList(),
+                                    hostnameActivity.entries.maxWith(Comparator { o1, o2 -> o1.value.compareTo(o2.value) })?.key
+                                            ?: "No activity")
+                            )
+                            fromHostnameList.clear()
+                            toHostnameList.clear()
+                            hostnameActivity.clear()
+                            lastReadTimeMs = clock.millis()
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+        running = true
+        followThread!!.start()
+    }
+
+    private fun readLine(inputStream: BufferedInputStream): String {
+        val line = StringBuffer()
+        loop@ while (inputStream.available() > 0) {
+            when (val char = inputStream.read().toChar()) {
+                '\r' -> continue@loop
+                '\n' -> break@loop
+                else -> line.append(char)
+            }
+        }
+        return line.toString()
+    }
+
+    private fun parseLine(line: String, hostname: String, toHostnameList: MutableSet<String>, fromHostnameList: MutableSet<String>, hostnameActivity: MutableMap<String, Int>) {
+        val logLine = LogLine.fromString(line)
+        if (logLine.receiverHostname == hostname) {
+            toHostnameList.add(logLine.senderHostname)
+        } else if (logLine.senderHostname == hostname) {
+            fromHostnameList.add(logLine.receiverHostname)
+        }
+        hostnameActivity.computeIfPresent(logLine.senderHostname) { _, value -> value + 1 }
+        hostnameActivity.putIfAbsent(logLine.senderHostname, 1)
+    }
+
+    private fun firstRead(inputStream: BufferedInputStream, hostname: String) {
+        val fromHostnameList = HashSet<String>()
+        val toHostnameList = HashSet<String>()
+        val hostnameActivity = HashMap<String, Int>()
+        var line = StringBuffer()
+        loop@ while (inputStream.available() > 0) {
+            when (val char = inputStream.read().toChar()) {
+                '\r' -> continue@loop
+                '\n' -> {
+                    if (line.toString() != "") {
+                        parseLine(line.toString(), hostname, toHostnameList, fromHostnameList, hostnameActivity)
+                    } else {
+                        break@loop
+                    }
+                    line = StringBuffer()
+                }
+                else -> line.append(char)
+            }
+        }
+        resultQueue.put(FollowResult(
+                fromHostnameList.toList().sorted(),
+                toHostnameList.toList().sorted(),
+                hostnameActivity.entries.maxWith(Comparator { o1, o2 -> o1.value.compareTo(o2.value) })!!.key)
+        )
+    }
+
+    fun stop() {
+        if (followThread != null) {
+            followThread!!.interrupt()
+            followThread!!.join()
+        }
     }
 }
--- src/com/thiamath/logparser/app/test/FollowTest.kt
+++ src/com/thiamath/logparser/app/test/FollowTest.kt
@@ -3,10 +3,15 @@ package com.thiamath.logparser.app.test
 import com.thiamath.logparser.app.follow.FollowManagerImpl
 import com.thiamath.logparser.app.model.FollowResult
 import java.io.File
+import java.io.FileWriter
+import java.time.Clock
+import java.time.Duration
+import java.time.Instant
+import java.time.ZoneId
 import java.util.concurrent.LinkedBlockingDeque
 import kotlin.test.assertEquals
 
-private const val TEST_RESOURCES_PATH = "resources/test/parserTest"
+private const val TEST_RESOURCES_PATH = "resources/test/followTest"
 
 object FollowTest : Test {
     override fun launchTest() {
@@ -16,36 +21,54 @@ object FollowTest : Test {
 
     object FollowManagerImplTest : Test {
         override fun launchTest() {
+            val testFilename = "$TEST_RESOURCES_PATH/FollowManagerImplHappyPathTest.txt"
+            // Setup
+            File(testFilename).writeText("""
+                    1586166210000 Peter Alice
+                    1586166220000 Peter Perry
+                    1586166240000 Perry Bob
+                    1586166321000 Alice Peter
+                """.trimIndent().plus("\n"))
             // Given
             val testQueue = LinkedBlockingDeque<FollowResult>()
-            val followManager = FollowManagerImpl(testQueue, resultIntervalMs = 4000)
+            val clock = Clock.fixed(Instant.now(), ZoneId.systemDefault())
+            val followManager = FollowManagerImpl(testQueue, resultIntervalMs = 4000, clock = clock)
             // When
-            val followThread = Thread {
-                followManager.followParseFile("$TEST_RESOURCES_PATH/FollowManagerImplHappyPathTest.txt", "Peter")
-            }
-            followThread.start()
-            // Then
-            assertEquals(testQueue.count(), 1, "The first round must have only one result on queue.")
-            assertEquals(testQueue.first, FollowResult(listOf("Alice", "Perry"), listOf("Alice"), "Peter"), "The queued result must satisfy.")
-            val producerThread = Thread {
-                File("$TEST_RESOURCES_PATH/FollowManagerImplHappyPathTest.txt").bufferedWriter().use {
-                    it.append("1586167210000 Alice Bob\n" +
-                            "1586167220000 Alice Perry\n" +
-                            "1586167240000 Alice Bob\n" +
-                            "1586167239000 Peter Bob\n" +
-                            "1586167321000 Alice Peter")
+            try {
+                followManager.followParseFile(testFilename, "Peter")
+                // Then
+                Thread.sleep(500)
+                assertEquals(1, testQueue.count(), "The first round must have one result on queue")
+                assertEquals(FollowResult(
+                        fromHostnameList = listOf("Alice", "Perry"),
+                        toHostnameList = listOf("Alice"),
+                        mostActiveHostname = "Peter"
+                ), testQueue.first, "The queued result must satisfy on first round")
+                val producerThread = Thread {
+                    val file = File(testFilename)
+                    FileWriter(file, true).buffered().use {
+                        it.appendln("1586167210000 Alice Bob")
+                        it.appendln("1586167220000 Alice Perry")
+                        it.appendln("1586167240000 Alice Bob")
+                        it.appendln("1586167239000 Peter Bob")
+                        it.appendln("1586167321000 Alice Peter")
+                    }
                 }
+                producerThread.start()
+                assertEquals(1, testQueue.count(), "The second round must have only one result on queue")
+                assertEquals(FollowResult(listOf("Alice", "Perry"), listOf("Alice"), "Peter"), testQueue.first, "The queued result must satisfy on second round")
+                Thread.sleep(1000)
+                followManager.clock = Clock.offset(clock, Duration.ofSeconds(5))
+                Thread.sleep(1000)
+                assertEquals(2, testQueue.count(), "The third round must have two results on queue")
+                testQueue.pollFirst()
+                assertEquals(FollowResult(listOf("Bob"), listOf("Alice"), "Alice"), testQueue.first, "The queued result must satisfy on third round")
+                producerThread.join()
+            } finally {
+                // cleanup
+                followManager.stop()
+                File(testFilename).delete()
             }
-            producerThread.start()
-            assertEquals(testQueue.count(), 1, "The second round must have only one result on queue.")
-            assertEquals(testQueue.first, FollowResult(listOf("Alice", "Peter"), listOf("Alice"), "Peter"), "The queued result must satisfy.")
-            Thread.sleep(5000)
-            assertEquals(testQueue.count(), 2, "The third round must have two results on queue.")
-            testQueue.pollFirst()
-            assertEquals(testQueue.first, FollowResult(listOf("Bob"), listOf("Alice"), "Alice"), "The queued result must satisfy.")
-            followThread.interrupt()
-            producerThread.join()
-            followThread.join()
         }
     }
 }
